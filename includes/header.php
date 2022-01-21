72ee585e7b596f96fa430be818b7eb8c3a52da1f
lcdmonitor <dave@davidtsimmons.com>
2022-01-21 09:49:09 -0500

    updated list icon size to svg and 32x32

--- includes/header.php
+++ includes/header.php
@@ -26,7 +26,11 @@
             <a href="/" class="text-decoration-none text-white fs-5 fw-bold">Florida Launch Alliance</a>
 
             <div>
-                <button class="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="#navbar" aria-expanded="false" aria-label="toggle navigation"><i class="bi bi-list" style="font-size: 1rem;"></i></button>
+                <button class="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="#navbar" aria-expanded="false" aria-label="toggle navigation">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+</svg>
+                </button>
             </div>
         </div>
     </header>
