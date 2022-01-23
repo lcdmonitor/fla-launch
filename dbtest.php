e8690c95be0b2e5593afb50e48b51f044c76470c
Dave S <dave@davidtsimmons.com>
2022-01-23 05:59:05 -0500

    added bb code sample

--- dbtest.php
+++ dbtest.php
@@ -3,6 +3,18 @@
 
 
 <?php
+$parser = new JBBCode\Parser();
+$parser->addCodeDefinitionSet(new JBBCode\DefaultCodeDefinitionSet());
+
+
+$text = "The default codes include: [b]bold[/b], [i]italics[/i], [u]underlining[/u], ";
+$text .= "[url=http://jbbcode.com]links[/url], [color=red]color![/color] and more.";
+
+$parser->parse($text);
+
+print $parser->getAsHTML();
+
 print '<br>';
+
 ListRoles();
 ?>
