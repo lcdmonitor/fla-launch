8df4188857735021518a4244f069c771f8e9482c
lcdmonitor <dave@davidtsimmons.com>
2022-02-24 10:30:43 -0500

    added email capability, todo item 0

--- _config/config.inc.php
+++ _config/config.inc.php
@@ -6,4 +6,11 @@ $DB_NAME="web";
 $DB_PORT="8889";
 $DB_USER="WEBUSER";
 $DB_PASS="ABCD1234";
+
+$EMAIL_SERVER="smtp.office365.com";
+$EMAIL_USER="support@flalaunch.com";
+$EMAIL_PASSWORD="?{3uEm2,XHg+";
+$EMAIL_FROM="support@flalaunch.com";
+$EMAIL_FROM_NAME="Florida Launch Alliance Support";
+$EMAIL_DEBUG=false;
 ?>
