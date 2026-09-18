46e3854621d6fc7ee4a08f3c04759d35070fd3b5
David Simmons <dave@davidtsimmons.com>
2026-09-17 22:54:38 -0400

    updated font face

--- includes/header.php
+++ includes/header.php
@@ -24,7 +24,7 @@ try {
     <!-- Google Fonts-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
 
     <!-- favico -->
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
